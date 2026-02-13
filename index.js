fd15dd679e3d4d383bc6b33d5e6bbca9de9e47a2
elizabetheonoja-art <elizabetheonoja@gmail.com>
2026-02-13 15:28:32 +0000

    init: setup Utility API

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.json({ 
+    project: 'Utility Drip', 
+    status: 'Monitoring Meters', 
+    contract: 'CB7PSJZALNWNX7NLOAM6LOEL4OJZMFPQZJMIYO522ZSACYWXTZIDEDSS' 
+  });
+});
+
+app.listen(port, () => console.log('Utility API running'));
